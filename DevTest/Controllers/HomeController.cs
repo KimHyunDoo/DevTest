336da33441a83ae137e5d812521d7c4aea0d1c23
KimHyunDoo <kp1376@naver.com>
2021-08-27 14:18:22 +0900

    Git Start!

---
+++ DevTest/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PDF_File_Test.Models;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.Text;
+
+namespace PDF_File_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Chart()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult FileTest(string file)
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+            file = @"C:\Users\KimHyunJoo\Desktop\TESTPDF.pdf";
+            
+            using (PdfReader reader = new PdfReader(file))
+            {
+                for (int pageNo = 1; pageNo <= reader.NumberOfPages; pageNo++)
+                {
+                    ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                    string text = PdfTextExtractor.GetTextFromPage(reader, pageNo, strategy);
+                    text = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
+                    sb.Append(text);
+                }
+            }
+
+
+
+            return Json(new { result = sb.ToString() });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
